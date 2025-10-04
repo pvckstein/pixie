b257d8822160b2821ef042e5218ee2ec5f82ce5d
pvckstein <160131995+pvckstein@users.noreply.github.com>
2025-10-04 12:27:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-// src/index.js
-import { loadDoT, applyDoTPatch } from './engine.js';
-const doT = await loadDoT();
-applyDoTPatch(doT);
-import './components/popover.js';
-import './components/collapse.js';
 
